9b3f684456270dbef2d3fdfef7cb3bd592df4aca
Hardikgojiya <140227897+hardik-gojiya@users.noreply.github.com>
2025-04-09 12:13:16 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
 
         <a
           title="Resume Download"
-          href={myImage}
+          href="/"
           download="Hardik's resume"
           className="inline-flex items-center gap-2 px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition"
         >
