92d94f77349d1c92586c1f46f493d82d6eb037a4
Hardikgojiya <hardikgojiya143@gmail.com>
2025-04-11 10:12:13 +0530

    ..

--- src/Components/ProjectTemp.jsx
+++ src/Components/ProjectTemp.jsx
@@ -62,7 +62,7 @@ function ProjectTemp() {
                   rel="noopener noreferrer"
                   className="w-full px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition"
                 >
-                  Live Demo
+                  Visit Site
                 </a>
               )}
             </div>
