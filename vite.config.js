5ebdfbee08e9da7c3d94b54d71a1cff22ec7dbd8
Hardikgojiya <hardikgojiya143@gmail.com>
2025-12-29 11:31:59 +0530

    desktop application

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Hardik Portfolio",
+        short_name: "Portfolio",
+        display: "standalone",
+        theme_color: "#000000",
+      },
+    }),
+  ],
   base: "/",
 });
